271e49fc6d5f8a461fb30d526bc512a8bc60c657
Davi Bernardo <dnssites@gmail.com>
2026-01-30 19:23:46 -0300

    Melhorias no irrf nos relatórios regionais e contabilidade

--- app/Http/Controllers/ContabilidadeController.php
+++ app/Http/Controllers/ContabilidadeController.php
@@ -10,6 +10,7 @@ class ContabilidadeController extends Controller
     public function irrf(Request $request)
     {
         $data = app(IrrfServices::class)->execute($request->all());
+        
         if($request->route()->uri() == 'contabilidade/irrf'){
             return view('contabilidade.irrf.index-contabilidade', $data);
         }else{
--- resources/views/contabilidade/irrf/index.blade.php
+++ resources/views/contabilidade/irrf/index.blade.php
@@ -75,6 +75,7 @@
                     <th>PREBENDAS</th>
                     <th>Nº DEPENDENTES</th>
                     <th>BASE DE CÁLCULOS</th>
+                    <th>REDUTOR</th>
                     <th>IRRF CALCULADO</th>
                     <th>RETIDO</th>
                     <th>REPASSADO</th>
@@ -88,6 +89,7 @@
                     <td>R$ {{ number_format($item['prebanda']->valor_prebendas, 2, ',', '.') }}</td>
                     <td>{{ $item['prebanda']->n_dependentes }}</td>
                     <td>R$ {{ number_format($item['imposto']->valorBase, 2, ',', '.') }}</td>
+                    <td>R$ {{ number_format($item['imposto']->valorRedutor, 2, ',', '.') }}</td>
                     <td>R$ {{ number_format($item['imposto']->valorImposto, 2, ',', '.') }}</td>
                     <td>R$ {{ number_format($item['prebanda']->retido, 2, ',', '.') }}</td>
                     <td>R$ {{ number_format($item['prebanda']->repasse, 2, ',', '.') }}</td>
