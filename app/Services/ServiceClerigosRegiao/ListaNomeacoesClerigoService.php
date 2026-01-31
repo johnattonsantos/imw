0c02dd322065034b2a2de564ab608225968a3ebe
Davi Bernardo <dnssites@gmail.com>
2026-01-30 22:09:53 -0300

    Melhorias na listagem de nomeações da instituição

--- app/Services/ServiceClerigosRegiao/ListaNomeacoesClerigoService.php
+++ app/Services/ServiceClerigosRegiao/ListaNomeacoesClerigoService.php
@@ -40,7 +40,8 @@ class ListaNomeacoesClerigoService
             ->with('funcaoministerial')
             ->with('pessoa')
             ->with('instituicao')
-            ->orderBy('pessoas_nomeacoes.data_nomeacao', 'DESC')
+            ->orderByRaw('data_termino IS NULL DESC')
+            //->orderBy('pessoas_nomeacoes.data_nomeacao', 'DESC')
             ->orderBy('pessoas_pessoas.nome', 'ASC')
             ->get();
         return [
