167f27eda04fdd0d65a0804b29b2c2a8f51e75c5
ntzn7 <ntz200823@gmail.com>
2026-02-27 18:31:42 -0300

    relatorio distrital clerigos

---
+++ docs/sql/relatorio-distrital-clerigos-aniversariantes.sql
@@ -0,0 +1,9 @@
+INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'distrito-clerigos-aniversariantes', current_timestamp(), current_timestamp(), NULL);
+
+INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
+SELECT NULL, '2', r.id, current_timestamp(), current_timestamp()
+FROM regras r
+WHERE r.nome = 'distrito-clerigos-aniversariantes'
+ORDER BY r.id DESC
+LIMIT 1;
+
