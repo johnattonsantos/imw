f1eb0c4293c709995ebc27e39c59be9f0bbfb24b
Davi Bernardo <dnssites@gmail.com>
2026-03-13 15:42:52 -0300

    Melhorias na função cotasOrcamentarias igreja local, distrito e região

--- app/Traits/FinanceiroUtils.php
+++ app/Traits/FinanceiroUtils.php
@@ -185,7 +185,7 @@ trait FinanceiroUtils
                         select(
                             DB::raw("(SELECT SUM(valor) FROM financeiro_lancamentos fl
                                 JOIN financeiro_plano_contas fpc ON fpc.id = fl.plano_conta_id
-                                WHERE fpc.numeracao in  ('1.01.01', '1.01.02', '1.02.01') AND fl.instituicao_id = $instituicao_id $sqlDataMovimento AND conciliado = 1 AND fl.deleted_at is null) AS dizimos_ofertas"),
+                                WHERE fpc.numeracao in ('1.01.01', '1.01.02', '1.02.01') AND fl.instituicao_id = $instituicao_id $sqlDataMovimento AND conciliado = 1 AND fl.deleted_at is null) AS dizimos_ofertas"),
                             DB::raw("(SELECT SUM(valor) FROM financeiro_lancamentos fl
                                 JOIN financeiro_plano_contas fpc ON fpc.id = fl.plano_conta_id
                                 WHERE fpc.numeracao in ('2.18.23') AND fl.instituicao_id = $instituicao_id $sqlDataMovimento AND conciliado = 1 AND fl.deleted_at is null)  AS dizimos_pastoral_fiw")
