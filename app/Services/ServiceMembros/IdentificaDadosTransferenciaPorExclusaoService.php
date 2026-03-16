fce89025adaf13d4a24cab98d194d4d78564ac04
ntzn7 <ntz200823@gmail.com>
2026-03-16 13:52:26 -0300

    Melhorias ao transferir membros e relatórios de membros e clerigos por igrejas

--- app/Services/ServiceMembros/IdentificaDadosTransferenciaPorExclusaoService.php
+++ app/Services/ServiceMembros/IdentificaDadosTransferenciaPorExclusaoService.php
@@ -26,11 +26,16 @@ class IdentificaDadosTransferenciaPorExclusaoService
             ->where('instituicao_pai_id', 23);
 
         // Consulta principal
-        return InstituicoesInstituicao::whereIn('instituicao_pai_id', $subQuery)
-            ->where('ativo', 1)
-            ->where('tipo_instituicao_id', InstituicoesTipoInstituicao::IGREJA_LOCAL)
-            ->where('id', '<>', Identifiable::fetchSessionIgrejaLocal()->id)
-            ->orderBy('nome', 'asc')
+        return InstituicoesInstituicao::query()
+            ->leftJoin('instituicoes_instituicoes as distrito', 'distrito.id', '=', 'instituicoes_instituicoes.instituicao_pai_id')
+            ->whereIn('instituicoes_instituicoes.instituicao_pai_id', $subQuery)
+            ->where('instituicoes_instituicoes.ativo', 1)
+            ->where('instituicoes_instituicoes.tipo_instituicao_id', InstituicoesTipoInstituicao::IGREJA_LOCAL)
+            ->where('instituicoes_instituicoes.id', '<>', Identifiable::fetchSessionIgrejaLocal()->id)
+            ->select('instituicoes_instituicoes.*')
+            ->with('instituicaoPai:id,nome')
+            ->orderBy('distrito.nome', 'asc')
+            ->orderBy('instituicoes_instituicoes.nome', 'asc')
             ->get();
     }
 }
