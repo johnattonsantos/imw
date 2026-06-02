4057350316d9a171daeecda37c21860171fb2c6b
Davi Bernardo <dnssites@gmail.com>
2026-06-02 14:17:40 -0300

    Melhorias nos coordenadores regionais ebd, gceu

--- docs/sql/ebd_coordenador_regional.sql
+++ docs/sql/ebd_coordenador_regional.sql
@@ -1,4 +1,4 @@
-INSERT INTO `perfils` (`id`, `nome`, `nivel`, `created_at`, `updated_at`, `deleted_at`) VALUES ('26', 'Coordenador Regional EBD', 'I', current_timestamp(), current_timestamp(), NULL);
+INSERT INTO `perfils` (`id`, `nome`, `nivel`, `created_at`, `updated_at`, `deleted_at`) VALUES ('26', 'Coordenador Regional EBD', 'R', current_timestamp(), current_timestamp(), NULL);
 
 INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'regiao-menu-relatorio-ebd', current_timestamp(), current_timestamp(), NULL);
 
@@ -15,17 +15,6 @@ FROM regras r
 WHERE r.nome = 'regiao-menu-relatorio-ebd'
 ORDER BY r.id DESC
 LIMIT 1;
-
-
-
-INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'regiao-menu-relatorio-financeiro', current_timestamp(), current_timestamp(), NULL);
-INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
-SELECT NULL, '3', r.id, current_timestamp(), current_timestamp()
-FROM regras r
-WHERE r.nome = 'regiao-menu-relatorio-financeiro'
-ORDER BY r.id DESC
-LIMIT 1;
-
 
 
 
--- docs/sql/gceu_coordenador_regional.sql
+++ docs/sql/gceu_coordenador_regional.sql
@@ -1,4 +1,23 @@
-INSERT INTO `perfils` (`id`, `nome`, `nivel`, `created_at`, `updated_at`, `deleted_at`) VALUES ('27', 'Coordenador Regional GCEU', 'I', current_timestamp(), current_timestamp(), NULL);
+INSERT INTO `perfils` (`id`, `nome`, `nivel`, `created_at`, `updated_at`, `deleted_at`) VALUES ('27', 'Coordenador Regional GCEU', 'R', current_timestamp(), current_timestamp(), NULL);
+
+
+INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'regiao-menu-relatorio-financeiro', current_timestamp(), current_timestamp(), NULL);
+INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
+SELECT NULL, '3', r.id, current_timestamp(), current_timestamp()
+FROM regras r
+WHERE r.nome = 'regiao-menu-relatorio-financeiro'
+ORDER BY r.id DESC
+LIMIT 1;
+
+INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'relatorio-clerigos-dados', current_timestamp(), current_timestamp(), NULL);
+INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
+SELECT NULL, '3', r.id, current_timestamp(), current_timestamp()
+FROM regras r
+WHERE r.nome = 'relatorio-clerigos-dados'
+ORDER BY r.id DESC
+LIMIT 1;
+
+
 
 INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
 SELECT NULL, '27', r.id, current_timestamp(), current_timestamp()
