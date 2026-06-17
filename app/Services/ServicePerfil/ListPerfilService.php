9873c0afd6415517c305c4ed97557ff662a55c35
Davi Bernardo <dnssites@gmail.com>
2026-06-17 16:10:56 -0300

    Colocando a carteira digital por variavel de região

--- app/Services/ServicePerfil/ListPerfilService.php
+++ app/Services/ServicePerfil/ListPerfilService.php
@@ -45,6 +45,7 @@ class ListPerfilService
 
         $pessoa['nome_regiao'] = optional($instituicao)->nome ?? '';
         $pessoa['nome_regiao_formatado'] = $this->formatRegionName($pessoa['nome_regiao']);
+        $pessoa['telefone_sede_administrativa'] = $this->formatInstitutionPhone($instituicao);
 
         if ($pessoa->foto) {
             $pessoa->foto = $this->resolveFotoUrl((string) $pessoa->foto);
@@ -100,5 +101,21 @@ class ListPerfilService
         }
 
         return mb_convert_case($regionName, MB_CASE_TITLE, 'UTF-8');
+    }
+
+    private function formatInstitutionPhone(?InstituicoesInstituicao $instituicao): string
+    {
+        if (!$instituicao || empty($instituicao->telefone)) {
+            return '';
+        }
+
+        $telefone = preg_replace('/\D+/', '', (string) $instituicao->telefone);
+        $ddd = preg_replace('/\D+/', '', (string) $instituicao->ddd);
+
+        if ($ddd !== '' && strlen($telefone) <= 9) {
+            $telefone = $ddd . $telefone;
+        }
+
+        return formatarTelefone($telefone);
     }
 }
