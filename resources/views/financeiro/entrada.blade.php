e06375070ca15f2e6661c2069ca8c9d9362477e9
Davi Bernardo <dnssites@gmail.com>
2026-07-23 19:31:15 -0300

    Melhorias em Financeiro/movimento finanenceiro/Entradas

--- resources/views/financeiro/entrada.blade.php
+++ resources/views/financeiro/entrada.blade.php
@@ -236,7 +236,7 @@
         });
 
         // definir o idioma padrão do Select2 para português
-        5fn.select2.defaults.set("language", window.IMW_SELECT2_LANGUAGE || "pt-BR");
+        $.fn.select2.defaults.set("language", window.IMW_SELECT2_LANGUAGE || "pt-BR");
         $('#caixa_id').select2({
            placeholder: 'Selecione',
             allowClear: true
