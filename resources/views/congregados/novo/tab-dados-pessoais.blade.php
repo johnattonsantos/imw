09869451cec00d5bca1f0ebef8d9ec68082c46aa
ntzn7 <ntz200823@gmail.com>
2026-08-13 16:56:22 -0300

    retirando campos de membros

--- resources/views/congregados/novo/tab-dados-pessoais.blade.php
+++ resources/views/congregados/novo/tab-dados-pessoais.blade.php
@@ -478,15 +478,7 @@
               </div>
             </div>
             <div class="row mb-4">
-              <div class="col-lg-6">
-                <label for="historico">{{ __('Pastor Oficiante') }}</label>
-                <input type="text" class="form-control" id="historico" name="historico" value="{{ old('historico') }}">
-                @error('historico')
-                  <span class="help-block text-danger">{{ $message }}</span>
-                @enderror
-              </div>
-
-              <div class="col-lg-6">
+              <div class="col-lg-12">
                   <label class="control-label">{{ __('Congregação:') }}</label>
                   <select id="congregacao_id" name="congregacao_id" class="form-control @error('congregacao_id') is-invalid @enderror" >
                       <option value="" {{ old('congregacao_id') == '' ? 'selected' : '' }}>{{ __('Selecione') }}</option>
