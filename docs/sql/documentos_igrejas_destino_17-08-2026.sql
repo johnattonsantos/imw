52b420a5ae577e65b1944a4491cf7a828921a41e
Davi Bernardo <dnssites@gmail.com>
2026-08-17 18:38:55 -0300

    Trantando mensagem quando não houer conexão com S3

--- docs/sql/documentos_igrejas_destino_17-08-2026.sql
+++ docs/sql/documentos_igrejas_destino_17-08-2026.sql
@@ -12,19 +12,10 @@ ALTER TABLE `documentos_igrejas`
     ON DELETE SET NULL;
 
 
-
-INSERT INTO `regras` (`id`, `nome`, `created_at`, `updated_at`, `deleted_at`) VALUES (NULL, 'documentos-igrejas-gerenciar', current_timestamp(), current_timestamp(), NULL);
-
-INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
-SELECT NULL, '3', r.id, current_timestamp(), current_timestamp()
-FROM regras r
-WHERE r.nome = 'documentos-igrejas-gerenciar'
-ORDER BY r.id DESC
-LIMIT 1;
-
 INSERT INTO perfil_regra (id, perfil_id, regra_id, created_at, updated_at)
 SELECT NULL, '13', r.id, current_timestamp(), current_timestamp()
 FROM regras r
 WHERE r.nome = 'documentos-igrejas-gerenciar'
 ORDER BY r.id DESC
 LIMIT 1;
+
